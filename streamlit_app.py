9b838d0fe7c55b41ffd1c36fe925251f0d0f5cae
Vairamuthu Sornabarathi <60732601+svairamuthu@users.noreply.github.com>
2024-09-28 23:34:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+from langchain.llms import OpenAI
 
-st.title('🎈 App Name')
+st.title("🦜🔗 Ask vAIra")
 
-st.write('Hello world!')
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+
+with st.form("my_form"):
+    text = st.text_area("Enter text:", "What is Gen AI?")
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+    elif submitted:
+        generate_response(text)
